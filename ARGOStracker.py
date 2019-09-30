95da37fdedd5d9b0bfb3eee0c36b682f5077c03c
bep22 <bep22@duke.edu>
2019-09-30 12:10:38 -0400

    Create ARGOStracker.py

---
+++ ARGOStracker.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------
+
+# ARGOStracker.py
+
+#
+
+# Description: Parses a line of ARGOS tracking data 
+
+#
+
+# Created by: Brittany Pashkow (bep22@duke.edu)
+
+# Created on: Fall 2019
+
+#--------------------------------------------------------------
